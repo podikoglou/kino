1ebf593ab401eac6cdfd9bcec9e0a52d97fe704f
Alex <alex.podikoglou@gmail.com>
2026-03-09 00:26:05 +0200

    feat: opcodes in vm.cpp

--- src/vm.cpp
+++ src/vm.cpp
@@ -4,6 +4,14 @@
 #include <format>
 #include <iterator>
 #include <stdexcept>
+// Documented in ISA.MD
+constexpr uint8_t OP_PUSHC = 0x00;
+constexpr uint8_t OP_PUSHV = 0x01;
+constexpr uint8_t OP_STORE = 0x02;
+constexpr uint8_t OP_ADD = 0x06;
+constexpr uint8_t OP_SUB = 0x07;
+constexpr uint8_t OP_MULT = 0x08;
+constexpr uint8_t OP_DIV = 0x09;
 
 VM::VM() { this->reset(); }
 
