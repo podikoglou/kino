6fea780a3342c78f03765ef43192af8e30e24a89
Alex <alex.podikoglou@gmail.com>
2026-03-08 23:52:12 +0200

    feat: kv and stack

--- src/vm.hpp
+++ src/vm.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "value.hpp"
 #include <array>
 #include <cstddef>
 #include <cstdint>
+#include <stack>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 constexpr size_t MEMORY_SIZE = 2048;
@@ -11,6 +15,9 @@ constexpr size_t MAX_PROGRAM_SIZE = 128;
 class VM {
 public:
   std::array<uint8_t, MEMORY_SIZE> memory;
+
+  std::unordered_map<std::string, value> kv;
+  std::stack<value> stack;
 
   // an index to `memory`, pointing to the current instruction
   size_t pc;
